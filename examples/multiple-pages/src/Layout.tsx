ac1e77480c654e750e3ba329b7997782f3d7334b
Riri <Daydreamerriri@outlook.com>
2023-07-25 02:50:04 +0800

    chore: head example

--- examples/multiple-pages/src/Layout.tsx
+++ examples/multiple-pages/src/Layout.tsx
@@ -1,21 +1,25 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Head } from 'vite-react-ssg'
 import './layout.css'
 
 export default function Layout() {
+  const [state, setState] = useState(false)
+
   return (
     <>
-      {/* <Head>
+      <Head>
         <meta charSet="UTF-8" />
         <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>head test</title>
-      </Head> */}
+        <title>head test {state ? 'A' : 'B'}</title>
+        <body className={`body-class-in-head-${state ? 'a' : 'b'}`} />
+      </Head>
       <main>
         <h1 className="layout">Layout</h1>
         <Suspense>
           <Outlet />
         </Suspense>
+        <button onClick={() => setState(v => !v)}>layout change title</button>
       </main>
     </>
   )
