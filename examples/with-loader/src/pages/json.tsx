a3df3b2ee850dd1cb41e081ea650785e18d6407b
Riri <Daydreamerriri@outlook.com>
2024-07-25 13:27:36 +0800

    chore: modify example

--- examples/with-loader/src/pages/json.tsx
+++ examples/with-loader/src/pages/json.tsx
@@ -6,7 +6,8 @@ export default function Docs() {
   return (
     <>
       <div>{data.key}</div>
-      <pre>{data.packageJson}</pre>
+      {/* eslint-disable-next-line react-dom/no-dangerously-set-innerhtml */}
+      <div dangerouslySetInnerHTML={{ __html: data.packageCodeHtml }} style={{ textAlign: 'start' }}></div>
     </>
   )
 }
@@ -16,14 +17,17 @@ export const Component = Docs
 export const entry = 'src/pages/json.tsx'
 
 export async function loader() {
-  // If you use `import('../docs/test.json?raw')`, it will return a JSON string.
+  // The code here will not be executed on the client side, and the modules imported will not be sent to the client.
   const fs = (await import('node:fs'))
   const cwd = process.cwd()
   const json = (await import('../docs/test.json')).default
+
   const packageJson = await fs.promises.readFile(`${cwd}/package.json`, 'utf-8')
+  const { codeToHtml } = await import('shiki')
+  const packageJsonHtml = await codeToHtml(packageJson, { lang: 'json', theme: 'vitesse-light' })
 
   return {
     ...json,
-    packageJson,
+    packageCodeHtml: packageJsonHtml,
   }
 }
