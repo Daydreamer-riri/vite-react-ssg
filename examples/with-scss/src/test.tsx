9d50cf0fdfcbb8d5fa6dd9ebb3eddf0c7b9e52fb
Riri <Daydreamerriri@outlook.com>
2026-01-10 17:42:55 +0800

    chore: update deps

---
+++ examples/with-scss/src/test.tsx
@@ -0,0 +1,10 @@
+import styles from './style.module.css'
+
+console.log(styles.abc)
+
+/**
+ * - This is a test file.
+ *   - It is used to demonstrate how to import CSS modules in a TypeScript file.
+ *
+ */
+const a = 1
